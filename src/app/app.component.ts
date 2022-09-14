6b26dfe04a8cb9e2188b13530a73980995ce3587
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-14 12:09:50 +0300

    #3 added drop event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 import { Task } from './task/task';
 
@@ -18,5 +19,35 @@ export class AppComponent {
       title: 'Http',
       description: "Angular İle Http İsteği Yap"
     },
-  ]
+  ];
+
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void { }
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    //task geldiği listeye sürüklenmişse bir şey yapma
+    if (event.previousContainer === event.container) {
+      return;
+    }
+
+    if (event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+
+
+
+
+  }
+
+
+
+
 }
